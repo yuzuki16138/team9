5082594c3629471b973198365e2e5f4857e23467
tmy3018 <baske3018@gmail.com>
2020-10-15 10:23:08 +0900

    Dがキャラ移動処理を追加

--- team9/chara.java
+++ team9/chara.java
@@ -15,5 +15,9 @@ public class chara extends Actor
     public void act() 
     {
         // Add your action code here.
-    }    
+        if(Greenfoot.isKeyDown("left")){
+            setRotation(0);
+            move(-1);
+        } 
+    }
 }
