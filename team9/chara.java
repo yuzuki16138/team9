4f33dcc58f809326c03348b676a72bbb408fa90c
松田 柚希 <16138@kauth.local>
2020-10-15 10:21:25 +0900

    Aがキャラ移動処理を追加

--- team9/chara.java
+++ team9/chara.java
@@ -14,6 +14,10 @@ public class chara extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
-    }    
+        if( Greenfoot.isKeyDown( "up" ) ){
+        setRotation(270);
+        move(1);
+
+    }  
+}
 }
